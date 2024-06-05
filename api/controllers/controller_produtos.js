a503cce0ca6ed8803faac2c6485272dd7dd95dba
José Carlos <jcsantos.souza1988@gmail.com>
2024-06-05 20:10:19 -0300

    Criada a validaçao dos Dados close #29

--- api/controllers/controller_produtos.js
+++ api/controllers/controller_produtos.js
@@ -1,6 +1,12 @@
+const mongoose = require('mongoose');
+const Produto = require('../models/model_produtos');
 
-const acao = (req, res) => {
-
-};
-
-module.exports = { acao };
+async function validarDados(req, res, next) {
+    const produto = new Produto(req.body);
+    try {
+        await produto.validate();
+        next();
+    } catch (error) {
+        res.status(422).json({ msg: 'Dados de produto invalidos'});
+    };
+}
