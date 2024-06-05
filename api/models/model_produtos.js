e3ddec1e8bde2471649f3c76d31d0f1439e9f997
José Carlos <jcsantos.souza1988@gmail.com>
2024-06-05 19:57:51 -0300

    Model para Produto criado close #28

---
+++ api/models/model_produtos.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+const protudoSchema = new mongoose.Schema({
+    nome: {type: String, trim: true, uppercase: true, required: true},
+    preco: {type: Number, min: 0},
+});
+
+module.exports = mongoose.model('Produto', produtoSchema);
