ce98f2e9b853c99b5adbf93b7ae1bb1363e52ba0
Igor Almeida <igor3108almeida@gmail.com>
2024-06-04 21:44:57 -0300

    model para fornecedores criado close #47

--- api/controllers/controller_exemplo.js
+++ api/controllers/controller_exemplo.js
@@ -1,6 +1,14 @@
+const mongoose=require('mongoose');
+const Fornecedor = require('../models/model_fornecedor');
 
-const acao = (req, res) => {
-
+const validarDadosFornecedor = async (req, res, next) => {
+    const fornecedor = new Fornecedor(req.body);
+    try {
+        await fornecedor.validate();
+        next()
+    } catch (error) {
+        res.status(422).json({msg: "Dados do Fornecedor invalidos"});
+    }
 };
 
 module.exports = { acao };
